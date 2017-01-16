55fa46a69ba6656d00857553a2db78427a48d5c5
Matthew Ho <itsmatho@gmail.com>
2017-01-16 10:44:02 -0800

    session names now editable

--- popup/choose_tab_session.js
+++ popup/choose_tab_session.js
@@ -1,5 +1,6 @@
 var curr_session, saved_sessions;
 var session_count = 0;
+var editing = false;
 
 var session_option_elements = {};
 var sessions_list = document.querySelector('.sessions-list');
@@ -77,36 +78,84 @@ function deleteSession(session_name) {
   });
 }
 
-/* Display the window session options in the popup */
+/* Append the window session to the session list in the popup */
 function displaySessionOption(session_name, session) {
   var session_option = document.createElement('div');
   session_option_elements[session_name] = session_option;
   session_option.setAttribute('class', 'session');
 
+  var header_error_element = document.createElement('div');
+  header_error_element.setAttribute('class', 'error');
+
   var session_header = document.createElement('div');
   session_header.setAttribute('class', 'header');
   session_header.textContent = session_name;
 
-  //var urls_btn = document.createElement('button');
-  //var edit_btn = document.createElement('button');
-  var start_btn = document.createElement('button');
-  var delete_btn = document.createElement('button');
+  var session_header_edit_btn_container = document.createElement('div');
+  session_header_edit_btn_container.setAttribute('class', 'btn-container');
 
-  //urls_btn.addEventListener('click', () => {showUrls(session);});
-  console.log("session displayed");
-  console.log(session);
+  var session_header_edit_btn = document.createElement('button');
+  session_header_edit_btn.setAttribute('class', 'header-edit');
+  session_header_edit_btn.textContent = "Edit";
+
+  var session_header_canceledit_btn = document.createElement('button');
+  session_header_canceledit_btn.setAttribute('class', 'header-edit-cancel');
+  session_header_canceledit_btn.textContent = "Cancel";
+
+  session_header_edit_btn_container.appendChild(session_header_edit_btn);
+  session_header_edit_btn_container.appendChild(header_error_element);
+
+  session_header_edit_btn.addEventListener('click', () => {
+    console.log('click');
+    // Make sure that the new name for the session doesn't already exist
+    if(editing && session_header.textContent !== session_name && saved_sessions[session_header.textContent]) {
+      header_error_element.textContent = "The session name already exists! Please use another name.";
+      console.log('already exists');
+    } else {
+      console.log('editing...');
+      session_header.contentEditable = !session_header.isContentEditable;
+      session_header_edit_btn.textContent = session_header.isContentEditable ? "Done" : "Edit";
+
+      // If done editing, set the new name
+      if(editing && session_header.textContent !== session_name) {
+        session_option_elements[session_header.textContent] = session_option_elements[session_name];
+        if(!(delete session_option_elements[session_name])) console.log(session_name + " option element property not deleted!");
+        saved_sessions[session_header.textContent] = saved_sessions[session_name];
+        if(!(delete saved_sessions[session_name])) console.log(session_name + " saved session not deleted!");
+
+        session_name = session_header.textContent;
+        session = saved_sessions[session_name];
+        browser.storage.local.set({
+          'sessions': saved_sessions
+        });
+      }
+
+      console.log('editing status: ' + editing);
+      editing = !editing;
+      console.log('editing changed: ' + editing);
+    }
+    console.log('Editable status: ' + session_header.isContentEditable);
+  });
+
+  var start_btn = document.createElement('button');
   start_btn.addEventListener('click', () => {startSession(session);});
   start_btn.textContent = 'Start session';
   start_btn.setAttribute('class', 'start');
+
+  var delete_btn = document.createElement('button');
   delete_btn.addEventListener('click', () => {deleteSession(session_name);});
   delete_btn.textContent = 'Delete session';
   delete_btn.setAttribute('class', 'delete');
+
+  console.log("session displayed");
+  console.log(session);
 
   var session_separator = document.createElement('div');
   session_separator.setAttribute('class', 'panel-section-separator');
   
   session_option.appendChild(session_separator);
   session_option.appendChild(session_header);
+  session_option.appendChild(session_header_edit_btn);
   session_option.appendChild(start_btn);
   session_option.appendChild(delete_btn);
 
